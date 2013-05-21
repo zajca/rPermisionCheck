d6be368047e75fa83c5234c610fad75616a7c3c0
zajca <zajca@zajca.cz>
2013-05-21 13:31:39 +0200

    permission typo corection

--- utility.rb
+++ utility.rb
@@ -14,8 +14,8 @@ class Optparse
     options.look_at_level = 1
     options.default_directory = 444
     options.default_file = 555
-    options.change_permision = 0
-    options.log_file = "/var/log/watch_permisions.log"
+    options.change_permission = 0
+    options.log_file = "/var/log/watch_permissions.log"
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = "Usage: program [OPTIONS]"
@@ -23,8 +23,8 @@ class Optparse
       opts.separator  "Options"
 
       # Mandatory argument.
-      opts.on("-w","--write [bool]","1 => change permisions, 0 => log") do |write|
-        options.change_permision = write
+      opts.on("-w","--write [bool]","1 => change permissions, 0 => log") do |write|
+        options.change_permission = write
       end      
 
       opts.on("-c","--folder_to_check [folder]","which folder to scan !!Full path without slash at the end!!") do |folder_to_check|
@@ -33,7 +33,7 @@ class Optparse
 
       opts.on("-p","--look_at_level [level]",
                   [:first, :second],
-                  "how deep to look for permision.json (only first => ./permision.json./, second => ./*/permision.json)") do |look_at_level|
+                  "how deep to look for permission.json (only first => ./permission.json./, second => ./*/permission.json)") do |look_at_level|
         options.look_at_level = look_at_level
       end
 
@@ -78,13 +78,13 @@ end  # class Optparse
 
 class Checker
   #určuje defaultní souborovou masku
-  attr_accessor :DEFAULT_PERMISION_FILE
-  attr_accessor :DEFAULT_PERMISION_FOLDER
+  attr_accessor :DEFAULT_PERMISSION_FILE
+  attr_accessor :DEFAULT_PERMISSION_FOLDER
   # true = bude přepisovat oprávnění
-  attr_accessor :CHANGE_PERMISION
+  attr_accessor :CHANGE_PERMISSION
   # procházená složka
   attr_accessor :DEFAULT_DIRECTORY
-  # kde hledá permision.json
+  # kde hledá permission.json
   attr_accessor :LEVEL
   # adresa logu
   attr_accessor :log_file
@@ -92,12 +92,12 @@ class Checker
   
   # konstruktor
   def initialize(dpf, dpff, rewrite, dir, level,log)
-    @DEFAULT_PERMISION_FOLDER = dpf
-    @DEFAULT_PERMISION_FILE = dpff
+    @DEFAULT_PERMISSION_FOLDER = dpf
+    @DEFAULT_PERMISSION_FILE = dpff
     if rewrite
-      @CHANGE_PERMISION = true
+      @CHANGE_PERMISSION = true
     else
-      @CHANGE_PERMISION = false
+      @CHANGE_PERMISSION = false
     end
     @DEFAULT_DIRECTORY = dir
     @LEVEL = level
@@ -112,15 +112,14 @@ class Checker
     $logger.info("NEW PROGRAM RUN")
     $logger.info("***************")
     $logger.info("ARGS:")
-    $logger.info("default permisions for files #{@DEFAULT_PERMISION_FILE}")
-    $logger.info("default permisions for folders #{@DEFAULT_PERMISION_FOLDER}")
-    $logger.info("change permision #{@CHANGE_PERMISION}")
-    $logger.info("directory to check #{@DEFAULT_DIRECTORY}")
-    $logger.info("deep level check #{@LEVEL}")
+    $logger.info("default permissions for files: #{@DEFAULT_PERMISSION_FILE}")
+    $logger.info("default permissions for folders: #{@DEFAULT_PERMISSION_FOLDER}")
+    $logger.info("change permission: #{@CHANGE_PERMISSION}")
+    $logger.info("directory to check: #{@DEFAULT_DIRECTORY}")
+    $logger.info("deep level check: #{@LEVEL}")
   end # method initialize
 
   def run
-    $logger.info(@CHANGE_PERMISION)
     #defaultní cesta (vstup)
     Dir.chdir(@DEFAULT_DIRECTORY)
 
@@ -130,8 +129,8 @@ class Checker
     runScan(rootFolders,@DEFAULT_DIRECTORY)
   end # method run
 
-  #testování příznaku souborů určených v souboru permision.json
-  def isPermisionOK(file,permision,dirPath)
+  #testování příznaku souborů určených v souboru permission.json
+  def isPermisionOK(file,permission,dirPath)
     begin
         wr=`stat --format=%a #{dirPath}#{file}`.chomp
       rescue Exception=>e
@@ -139,14 +138,14 @@ class Checker
         $logger.fatal(e)
         exit
       else
-        if wr==permision
+        if wr==permission
           return true
         else
-          $logger.fatal("file #{dirPath}#{file} has #{wr} should be #{permision}")
-          if @CHANGE_PERMISION
-            $logger.warn("Changing permision of file #{dirPath}#{file} from  #{wr}")
+          $logger.fatal("file #{dirPath}#{file} has #{wr} should be #{permission}")
+          if @CHANGE_PERMISSION
+            $logger.warn("Changing permission of file #{dirPath}#{file} from  #{wr}")
             begin
-              `chmod #{permision} #{dirPath}#{file}`
+              `chmod #{permission} #{dirPath}#{file}`
             rescue Exception=>e
               $logger.fatal("external program thrown error")
               $logger.fatal(e)
@@ -158,7 +157,7 @@ class Checker
     end
   end # method isPermisionOK
 
-  #testování příznaku souborů neurčených v souboru permision.json
+  #testování příznaku souborů neurčených v souboru permission.json
   def isPermisionOKDefault(file,dirPath)
     begin
       wr=`stat --format=%a #{dirPath}#{file}`.chomp
@@ -169,19 +168,19 @@ class Checker
         exit
     else
       case wd
-         when "directory" then default_permision = @DEFAULT_PERMISION_FOLDER
-         when "regular file" then default_permision = @DEFAULT_PERMISION_FILE
+         when "directory" then default_permission = @DEFAULT_PERMISSION_FOLDER
+         when "regular file" then default_permission = @DEFAULT_PERMISSION_FILE
          else $logger.fatal("Invalid file type #{wd} #{dirPath}#{file}")
       end
   
-      if wr==default_permision
+      if wr==default_permission
         return true
       else
-        $logger.fatal("file #{dirPath}#{file} has #{wr} should be #{default_permision}")
-        if @CHANGE_PERMISION
-          $logger.warn("Changing permision of file #{dirPath}#{file} from  #{wr} to #{default_permision}")
+        $logger.fatal("file #{dirPath}#{file} has #{wr} should be #{default_permission}")
+        if @CHANGE_PERMISSION
+          $logger.warn("Changing permission of file #{dirPath}#{file} from  #{wr} to #{default_permission}")
             begin
-              `chmod #{default_permision} #{dirPath}#{file}`
+              `chmod #{default_permission} #{dirPath}#{file}`
             rescue Exception=>e
               $logger.fatal("external program thrown error")
               $logger.fatal(e)
@@ -193,10 +192,10 @@ class Checker
     end
   end # method isPermisionOKDefault
 
-  #Testování souborů určených v souboru permision.json
+  #Testování souborů určených v souboru permission.json
   def checkFoldersPermisionFile(dirPath, json, fileHash)
-    $logger.info("Check of files in permision.json")
-    json.each do |fileName, permision|
+    $logger.info("Check of files in permission.json")
+    json.each do |fileName, permission|
       begin
         wd=`stat --format=%F #{dirPath}#{fileName}`.chomp
        rescue Exception=>e
@@ -206,15 +205,15 @@ class Checker
       else
         if wd=="directory"
           Dir.chdir("#{dirPath}#{fileName}")
-          ifIsPermisionOK("#{fileName}",permision,dirPath)
+          ifIsPermisionOK("#{fileName}",permission,dirPath)
           fileHash["#{fileName}"]= true
           
           Dir.glob("**/*").each do |f| 
-            ifIsPermisionOK("#{fileName}/#{f}",permision,dirPath)
+            ifIsPermisionOK("#{fileName}/#{f}",permission,dirPath)
             fileHash["#{fileName}/#{f}"]= true
           end
         else
-          ifIsPermisionOK(fileName,permision,dirPath)
+          ifIsPermisionOK(fileName,permission,dirPath)
           fileHash["#{fileName}"]= true
         end
       end
@@ -223,22 +222,22 @@ class Checker
   end # method checkFoldersPermisionFile
 
   #opakující se dvojtá podmínka
-  def ifIsPermisionOK(fileName,permision,dirPath)
-    if !isPermisionOK(fileName,permision,dirPath)
-      if !isPermisionOK(fileName,permision,dirPath)
-        $logger.fatal("Can't chage permision of the file #{dirPath}#{fileName}")
+  def ifIsPermisionOK(fileName,permission,dirPath)
+    if !isPermisionOK(fileName,permission,dirPath)
+      if !isPermisionOK(fileName,permission,dirPath)
+        $logger.fatal("Can't chage permission of the file #{dirPath}#{fileName}")
       end
     end
   end # method ifIsPermisionOK
 
-  #Testování souborů neuvedených v souboru permision.json
+  #Testování souborů neuvedených v souboru permission.json
   def checkFoldersForDefaultPermision(dirPath, fileHash)
-    $logger.info("Check of files outside permision.json")
+    $logger.info("Check of files outside permission.json")
     fileHash.each do |fileName, isChecked| 
       if !isChecked
         if !isPermisionOKDefault(fileName,dirPath)
           if !isPermisionOKDefault(fileName,dirPath)
-            $logger.fatal("Can't chage permision of the file #{dirPath}#{fileName}")
+            $logger.fatal("Can't chage permission of the file #{dirPath}#{fileName}")
           end
         end
       end
@@ -259,18 +258,18 @@ class Checker
       fileHashTmp = Dir.glob("**/*").collect { |f| [f, false] }
       fileHash = Hash[fileHashTmp]
 
-      #testovaní existence permisions.json ve složce
+      #testovaní existence permissions.json ve složce
       begin
         #otevreni souboru nastaveni
-        file = File.open("#{dirPath}permisions.json", &:read)
+        file = File.open("#{dirPath}permissions.json", &:read)
       rescue Exception=>e
         $logger.fatal(e.message)
         $logger.warn("Skiping folder #{dirPath}")
       else
         json=JSON.parse(file)
-        #rutina pro veškerou práci se souborem permisions.json
+        #rutina pro veškerou práci se souborem permissions.json
         checkFoldersPermisionFile(dirPath, json, fileHash)
-        #rutina pro veškerou práci se soubory nedefinovanými v permision.json
+        #rutina pro veškerou práci se soubory nedefinovanými v permission.json
         checkFoldersForDefaultPermision(dirPath,fileHash)
       end
     end
@@ -279,6 +278,6 @@ end # class Checker
 
 
 options = Optparse.parse(ARGV)
-puts options
-checker = Checker.new(options.default_directory,options.default_file,options.change_permision,options.folder_to_check,options.look_at_level,options.log_file)
+
+checker = Checker.new(options.default_directory,options.default_file,options.change_permission,options.folder_to_check,options.look_at_level,options.log_file)
 checker.run()
